164e896a6bec407221627fe36184d0e74c201e11
ketakisolace1 <ketaki.solace@gmail.com>
2023-12-15 13:07:13 +0530

    route

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const Routes = () => {
+    return(
+        <>
+    <Header/>
+    <Footer/>
+    </>
+    );
+    
+};
+
+export default Routes;
